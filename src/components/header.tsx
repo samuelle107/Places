15a4245bf3cbbdcbdd2aef5fe3017285cb20ad54
samuelle107 <samuelle107@gmail.com>
2023-03-20 19:16:23 -0500

    zoom to places on search instead

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,48 @@ import SearchBar, { type SearchForm } from './search-bar';
 import { usePlaces } from '@/context/place-context';
 import { PlaceActionTypes } from '@/reducer/placeReducer';
 import getPlaces from '@/endpoints/places/getPlaces';
+import { placeToPosition, bboxToLngLatBounds } from '@/util/geospatial';
+import bbox from '@turf/bbox';
+import { useMap } from 'react-map-gl';
+import { type FeatureCollection } from 'geojson';
+import { type FSQPlace } from '@/models/foursquare';
 
 const Header: FC = () => {
   const { dispatch } = usePlaces();
+  const { 'explore-map': map } = useMap();
+
+  const zoomToPlaces = (places: FSQPlace[]): void => {
+    if (places.length === 0) return;
+
+    const fc: FeatureCollection = {
+      type: 'FeatureCollection',
+      features: places.map((place) => ({
+        id: place.fsq_id,
+        type: 'Feature',
+        properties: {},
+        geometry: {
+          type: 'Point',
+          // Remember, coordinates go lng lat
+          coordinates: placeToPosition(place),
+        },
+      })),
+    };
+
+    const bounds = bboxToLngLatBounds(bbox(fc));
+
+    try {
+      map?.fitBounds(bounds, {
+        padding: {
+          bottom: 200,
+          top: 200,
+          left: 400,
+          right: 400,
+        },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const handleSearch = async (data: SearchForm): Promise<void> => {
     try {
@@ -14,6 +53,8 @@ const Header: FC = () => {
       });
 
       const res = await getPlaces(data, undefined, 50);
+
+      zoomToPlaces(res.data.results);
 
       dispatch({
         type: PlaceActionTypes.FINISH_SEARCH,
